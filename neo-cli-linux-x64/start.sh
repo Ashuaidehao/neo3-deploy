8acaef0c552922474631ea3fb314a68859ad3283
Shuai <bfshm@qq.com>
2020-04-27 10:29:55 +0800

    update

--- neo-cli-linux-x64/start.sh
+++ neo-cli-linux-x64/start.sh
@@ -1,9 +1,4 @@
-curl --location --request POST 'http://128.1.61.238:33335' \
+curl --location --request POST 'http://127.0.0.1:30332' \
 --header 'Content-Type: application/json' \
 --header 'Content-Type: text/plain' \
---data-raw '{
-  "jsonrpc": "2.0",
-  "method": "openwallet",
-  "params": ["neo.db3","12345678"],
-  "id": 1
-}'
+--data-raw '{"jsonrpc": "2.0","method": "openwallet","params": ["neo.db3","12345678"],"id": 1}'
